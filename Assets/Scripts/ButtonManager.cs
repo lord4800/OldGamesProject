899627c0a834ca59880375e73e802cc5af00ed3b
lord4800 <killerontheroad4800@gmail.com>
2018-07-10 22:49:40 +0300

    изменил билдинг сетингс добавив туда две сущещствующие сцены, добавил реализацию легких кнопок атрибутом, и выпадающее и скрвающиеся меню

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -1,13 +1,24 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using EasyButtons;
 
 public class ButtonManager : MonoBehaviour {
 	[SerializeField]
 	string loadSceneByDef;
 
-	public void loadScene()
+    public bool TestScript;
+    [ConditionalHide("TestScript",true)]
+    public bool Stuff;
+    [ConditionalHide("TestScript", true)]
+    public AnimationCurve someCurve;
+
+    [Button]
+    public void SayHello()
+    {
+        Debug.LogError("Hello");
+    }
+    
+    public void loadScene()
 	{
 		SceneManager.LoadScene(loadSceneByDef);
 	}
